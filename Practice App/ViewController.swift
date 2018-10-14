21da04b4404f80538df44ccdbf090ad354c6de6b
Alexis Atianzar <a.atianzar@gmail.com>
2018-10-13 21:08:31 -0700

    Adding text fields 1,2

--- Practice App/ViewController.swift
+++ Practice App/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var theLabel: UILabel!
-    var tapCount = 0
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    
     @IBAction func buttonTapped(_ sender: Any) {
-        tapCount = tapCount + 1
-        if tapCount >= 10 {
-            theLabel.text = "You tapped the button 10 times!"
-        }
+        print(text1.text!)
+        print(text2.text!)
     }
     
     override func viewDidLoad() {
